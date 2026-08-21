a069efc798d56b0d15448339ff0f02f08a0341f8
VennusAmery <vennussalguero@gmail.com>
2026-08-21 10:54:08 -0600

    Agregar pr09

--- CalculadoraForms/CalculadoraForms/obj/Debug/net8.0-windows/CalculadoraForms.AssemblyInfo.cs
+++ CalculadoraForms/CalculadoraForms/obj/Debug/net8.0-windows/CalculadoraForms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculadoraForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35905e90f8cd031feba4de5cde1955baff5894b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d51a9ebf603d44bae52b0152a639d93c047a4f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculadoraForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculadoraForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
