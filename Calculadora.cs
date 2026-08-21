ca1cdcc3fff71cacdb5af489fa9a9882d0112f43
VennusAmery <vennussalguero@gmail.com>
2026-08-20 19:51:48 -0600

    Agregar Calculadora

---
+++ Calculadora.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Calculadora
+{
+    static void Main()
+    {
+        Console.Write("Primer número: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Operador (+ - * /): ");
+        string op = Console.ReadLine();
+
+        Console.Write("Segundo número: ");
+        double b = double.Parse(Console.ReadLine());
+
+        double resultado = op switch
+        {
+            "+" => a + b,
+            "-" => a - b,
+            "*" => a * b,
+            "/" => b != 0 ? a / b : double.NaN,
+            _ => double.NaN
+        };
+
+        Console.WriteLine($"Resultado: {resultado}");
+    }
+}
